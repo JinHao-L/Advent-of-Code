416d2aeee43782335f39a7ebcb58067041a6197a
Jin Hao <limjinhao@gmail.com>
2021-12-01 14:35:38 +0800

    Solve 2021 D1

---
+++ AOC-2021/Day1.js
@@ -0,0 +1,35 @@
+// Day 1: Sonar Sweep
+// https://adventofcode.com/2021/day/1
+
+const fs = require("fs");
+const file = fs.readFileSync("AOC-2021/Day1_input.txt");
+const inputArray = file.toString().split("\n").map((val) => parseInt(val));
+
+const testArray = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+function solvePart1(arr) {
+  let count = 0;
+  for(let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) count++;
+  }
+  return count;
+}
+
+console.log("Part 1: " + solvePart1(inputArray));
+
+function solvePart2(arr) {
+  let prevSum = 0;
+  for(let i = 0; i < 3; i++) {
+    prevSum += arr[i];
+  }
+
+  let count = 0;
+  for(let i = 3; i < arr.length; i++) {
+    const newSum = prevSum + arr[i] - arr[i - 3];
+    if (newSum > prevSum) count++;
+    prevSum = newSum;
+  }
+  return count;
+}
+
+console.log("Part 2: " + solvePart2(inputArray));
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Advent of Code
 This repository contains my AOC solutions for the following years:
 * AOC 2019 Quests (Incomplete)
-* AOC 2020 Quests (Ongoing)
+* AOC 2020 Quests (Completed)
+* AOC 2021 Quests (Ongoing)
 
 Check out the puzzles [here](https://adventofcode.com/)
